7edb997090e0130821ec5d5435de792ee3c0cc51
symiller <symiller@umich.edu>
2022-08-04 12:55:46 -0400

    bugs

--- get_daily_data.py
+++ get_daily_data.py
@@ -18,7 +18,7 @@ def get_data():
     player_teams = pd.read_csv('data/player_team_ids.csv')
 
     # load model
-    cl = pickle.load(open('SCOE_model.sav', 'rb'))
+    cl = pickle.load(open('scoe_model.pkl', 'rb'))
 
     # load players data
     players = chadwick_register()
